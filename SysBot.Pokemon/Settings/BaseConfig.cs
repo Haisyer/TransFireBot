eb8733e05029dc28778eaa889eb8af16d62539f4
Hasy <1729883690@qq.com>
2023-08-09 12:12:55 +0800

    Add settings to disable logging and control archive log count
    Update Core and ALM

--- SysBot.Pokemon/Settings/BaseConfig.cs
+++ SysBot.Pokemon/Settings/BaseConfig.cs
@@ -14,6 +14,12 @@ namespace SysBot.Pokemon
         [Category(FeatureToggle), Description("当启用时，机器人会在不处理任何东西时偶尔按下B按钮(以避免睡眠)。")]
         public bool AntiIdle { get; set; }
 
+        [Category(FeatureToggle), Description("启用文本日志,更改后需要重新启动才能生效。")]
+        public bool LoggingEnabled { get; set; } = true;
+
+        [Category(FeatureToggle), Description("要保留的旧文本日志文件的最大数量，将其设置为<=0可禁用日志清理。更改后需重新启动才能生效。")]
+        public int MaxArchiveFiles { get; set; } = 14;
+
         [Category(Debug), Description("在程序启动时跳过创建机器人；有助于测试集成。")]
         public bool SkipConsoleBotCreation { get; set; }
 
