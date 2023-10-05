8ca0b1574ce5b61c06d8fd95f2f5b7b93436d712
Revenzzz <121876802+Revenzzz@users.noreply.github.com>
2023-10-06 00:35:05 +0800

    Add files via upload

--- SysBot.Pokemon.Dodo/DodoBot.cs
+++ SysBot.Pokemon.Dodo/DodoBot.cs
@@ -55,8 +55,8 @@ namespace SysBot.Pokemon.Dodo
         {
             var channelId = Settings.ChannelId;
             if (string.IsNullOrWhiteSpace(channelId)) return;
-            //SendChannelMessage("开始派送！", channelId);
-            SendChannelMessageAll("开始派送！", channelId);
+            //SendChannelMessage("欢迎使用传火机器人！", channelId);
+            SendChannelMessageAll("欢迎使用传火机器人！", channelId);
             var Msg = "";
             if (!DodoBot<T>.Info.Hub.Config.Legality.AllowUseFile)
             {
